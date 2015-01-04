a7966a6dc0eb18013ba5e64d14cb3ca7f9792efa
Mike Wilcox <mike@mikewilcox.net>
2015-01-04 14:21:11 -0600

    update CSS

--- src/Grid.js
+++ src/Grid.js
@@ -13,11 +13,6 @@ define([
 		containerClass:'base-grid-container',
 		scrollerClass:'base-grid-scroller',
 		
-		templateString:'<div class="${baseClass}"><div class="base-grid-header-wrap"><div data-dojo-attach-point="header" class="${headerClass}"></div></div><div data-dojo-attach-point="container" class="${containerClass}"><div data-dojo-attach-point="scroller" class="${scrollerClass}"></div></div></div>',
-		rowLabelValueTemplate:'<div class="base-list-pair"><div class="base-list-label">{LABEL}</div><div class="base-list-text">{TEXT}</div></div>',
-		rowLabelTemplate:'<div class="base-list-pair"><div class="base-list-label">{LABEL}</div></div>',
-		rowValueTemplate:'<div class="base-list-pair"><div class="base-list-text">{TEXT}</div></div>',
-		
 		constructor: function(nodeId){
 			console.log('GRID!');
 			this.build(nodeId);
