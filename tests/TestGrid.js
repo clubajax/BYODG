539d4315c360b49ed97242925e97a162cf7ee6d2
Mike Wilcox <mike@mikewilcox.net>
2015-01-03 20:33:08 -0600

    update branch with params

--- tests/TestGrid.js
+++ tests/TestGrid.js
@@ -6,9 +6,9 @@ define([
 	
 	return declare({
 		declaredClass:'TestGrid',
-		constructor: function(nodeId){
+		constructor: function(options, nodeId){
 			this.buildGrid(nodeId);
-			this.loadData(function(data){
+			this.loadData(options, function(data){
 				this.render(data);
 			}.bind(this));
 		},
@@ -18,21 +18,30 @@ define([
 			this.grid.render(data.items);
 		},
 		
-		loadData: function(callback){
-			xhr('http://0.0.0.0:3000/?start=10&count=20&sort=index&dir=desc', function(data){
-				//data.items = data.items.map(function(item){
-				//	delete item.index;
-				//	return item;
-				//});
+		buildGrid: function(nodeId){
+			this.grid = new Grid(nodeId);
+		},
+		
+		loadData: function(options, callback){
+			var
+				url = 'http://0.0.0.0:3000/?',
+				params = [];
+				
+			options.start = options.start || 1;
+			options.count = options.count || 10;
+			
+			Object.keys(options).forEach(function(key){
+				params.push(key + '=' + options[key]);
+			});
+			
+			url += params.join('&');
+			
+			xhr(url, function(data){
 				console.table(data.items);
 				callback(data);
 			}, function(error){
 				console.error('error', error);
 			});		
-		},
-		
-		buildGrid: function(nodeId){
-			this.grid = new Grid(nodeId);
 		}
 	});
 	
